429da3ddac849b080995f2c15e22a8cb9dc3150d
ahmadbelalsidiqi <Ahmad.sidiqiNe@gmail.com>
2026-04-15 14:40:23 -0400

    Step 1 & 2, [average, high score, low score] complete.

--- src/main/java/com/pluralsight/TestStatistics.java
+++ src/main/java/com/pluralsight/TestStatistics.java
@@ -1,4 +1,23 @@
 package com.pluralsight;
 
 public class TestStatistics {
+    public static void main(String[] args) {
+        int[] testScores = {96, 93, 40, 100, 97, 92, 95, 98, 94, 90};
+        int highScore=0;
+        int lowScore= testScores[0];
+        int average=0;
+
+        for (int x : testScores){
+            average += x;
+            if (x>highScore){
+                highScore = x;}
+
+            if (x<lowScore) {
+                lowScore =x;}
+        }
+
+
+        average = average/testScores.length;
+        System.out.println("High score: "+highScore+ "\n low score: " + lowScore+ "\n Average score: " +average);
+    }
 }
