8d049015d18222f3d6dcb49c09dae7c0da5cc241
ahmadbelalsidiqi <Ahmad.sidiqiNe@gmail.com>
2026-04-15 14:56:42 -0400

    Median Added.

--- src/main/java/com/pluralsight/TestStatistics.java
+++ src/main/java/com/pluralsight/TestStatistics.java
@@ -1,11 +1,17 @@
 package com.pluralsight;
 
+import java.util.Arrays;
+
 public class TestStatistics {
     public static void main(String[] args) {
         int[] testScores = {96, 93, 40, 100, 97, 92, 95, 98, 94, 90};
         int highScore=0;
         int lowScore= testScores[0];
         int average=0;
+        int scoreLength = testScores.length;
+        double median;
+        int medianIndex = scoreLength/2;
+        Arrays.sort(testScores);
 
         for (int x : testScores){
             average += x;
@@ -13,11 +19,16 @@ public class TestStatistics {
                 highScore = x;}
 
             if (x<lowScore) {
-                lowScore =x;}
-        }
+                lowScore =x;} }
 
+        average = average/scoreLength;
 
-        average = average/testScores.length;
-        System.out.println("High score: "+highScore+ "\n low score: " + lowScore+ "\n Average score: " +average);
+        // Median is the middle dataset if there are two middle numbers then median is the average of those two.
+        if (scoreLength%2==0){
+            median = (double) (testScores[medianIndex-1] + testScores[medianIndex]) /2;
+        } else {
+            median = testScores[medianIndex]; }
+
+        System.out.println(" High score: "+highScore+ "\n low score: " + lowScore+ "\n Average score: " +average + "\n Median is: "+median);
     }
 }
