3e912be19c83381ed7b84714e146264c43bf2808
ahmadbelalsidiqi <Ahmad.sidiqiNe@gmail.com>
2026-04-15 15:49:27 -0400

    removed redundant(if-else) code.

--- src/main/java/com/pluralsight/TestStatistics.java
+++ src/main/java/com/pluralsight/TestStatistics.java
@@ -5,21 +5,13 @@ import java.util.Arrays;
 public class TestStatistics {
     public static void main(String[] args) {
         int[] testScores = {96, 93, 40, 100, 97, 92, 95, 98, 94, 90};
-        int highScore=0;
+        Arrays.sort(testScores);
+        int highScore=testScores.length-1;
         int lowScore= testScores[0];
-        int average=0;
+        double average=0;
         int scoreLength = testScores.length;
         double median;
         int medianIndex = scoreLength/2;
-        Arrays.sort(testScores);
-
-        for (int x : testScores){
-            average += x;
-            if (x>highScore){
-                highScore = x;}
-
-            if (x<lowScore) {
-                lowScore =x;} }
 
         average = average/scoreLength;
 
